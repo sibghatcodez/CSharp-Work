b703c50d12bdcba186eea6464c01403614626599
sibghatdev81 <118879569+sibghatcodez@users.noreply.github.com>
2023-09-26 16:17:46 +0500

    Create Day_8.cs

---
+++ Day_8.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Day8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //day 8th
+
+
+            //Task: Write a C# Sharp program to check if a given string contains only lowercase or uppercase characters
+            //Status: PASSED! RESPECT++; 1 
+
+
+
+            // string str = "ionlyeatpeanuts";
+            // string str_1 = "I ONLY EAT APPLES";
+            // {
+            //   if (str == str.ToLower())
+            // {
+            //    Console.WriteLine(str + " contains lowercase letters only!");
+            //    }
+            //   else Console.WriteLine("ily but it doesnt contain lower case letters.... UwU");
+            //}
+
+            // {
+            // if (str_1 == str_1.ToUpper())
+            //{
+            //    Console.WriteLine(str_1 + " contains uppercase letters only!");
+            // }
+            // else Console.WriteLine("ily but it doesnt contain upper case letters.... UwU");
+            //}
+
+
+            //Task: Write a C# Sharp program to remove the first and last elements from a given string.
+            //Status: EASY PEASY LEMONS QUEEZY PASSED! 2
+
+            //string str_2 = "LavaScript";
+            //Console.WriteLine(str_2.Substring(1,str_2.Length-2));
+
+
+            //Task: Write a C# Sharp program to check if a given string contains two similar consecutive letters.
+            //Status: Opposite of EZPZ but Passed. 3
+
+
+            //string str_3 = "Levvi Ackerman";
+            //string str_4 = "Ali Bin Ashfaq";
+
+
+            //            for(int i = 0; i < str_4.Length-1; i++)
+            //          {
+            //                if (str_4[i] == str_4[i + 1])
+            //              {
+            //                Console.WriteLine("true");
+            //          }
+            //}
+
+
+            //Task: Write a C# Sharp program to check the length of a given string is odd or even.
+            //Return 'Odd length' if the string length is odd otherwise 'Even length'. 4
+
+            //Status: Passed! yay
+
+            //string str_5 = "Muhammed Bin Qasim";
+            //int x = str_5.Length;
+
+            //Console.WriteLine(x);
+            //bool a = x % 2 == 0;
+            //if(!a)
+            //{
+            //   Console.WriteLine(a + " is Odd number.");
+            //} else Console.WriteLine(a + " is a even number.");
+
+
+
+
+            //Task: Write a C# Sharp program to check whether a word is plural or not
+            //Status: Passed-! 5
+
+            //string str_6 = "Books";
+            //string str_7 = "tables";
+            //string str_8 = "book";
+
+            //bool isPlural = str_8[str_8.Length - 1] == 's' ? true : false;
+            //if(isPlural) Console.WriteLine(str_8 + " is Plural");
+            //else Console.WriteLine("It isnt plural!");
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
