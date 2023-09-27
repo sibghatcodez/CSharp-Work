8d591c99b1b9c4ce46b5958593adb5e8e434fda7
sibghatdev81 <118879569+sibghatcodez@users.noreply.github.com>
2023-09-27 16:35:15 +0500

    Create Day_9.cs

---
+++ Day_9.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Day_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //dath 9th!
+
+            //Task: 79. Write a C# Sharp program to convert an integer to a string and a string to an integer.
+            //Status: Partially passed
+
+            //int a = 1;
+            //string b = a.ToString();
+            //Console.WriteLine(b.GetType());
+
+            //string c = "56";
+            //int d = int.Parse(c);
+            //Console.WriteLine(d.GetType());
+
+
+
+            //Task: 80. Write a C# Sharp program to convert all the values of a given array of mixed values to string values.
+            //Status: failed.
+
+            //int[] array_1 = { 1 };
+            //string[] array_2 = { "50" };
+            //  bool[] array_3 = { false };
+            //    double[] array_4 = { 1010.1010 };
+
+
+            //      string e = Array.ConvertAll<x => x.toString();
+            //        string f = array_2.ToString();
+            //          string g = bool.Parse(array_3);
+            //            string h = double.Parse(array_4);
+
+            //Console.WriteLine($"{e} {f} {g} {h}");
+
+
+
+            //Task: 81. Write a C# Sharp program to swap a two-digit number and
+            //check whether the given number is greater than its swap value.
+            //Status: Passed...!!!
+
+            //int number = 24;
+            //string numb = number.ToString();
+            //string n1 = numb[0].ToString();
+            //string n2 = numb[1].ToString();
+            //string numb2 = n2+n1;
+            //int number_2 = int.Parse(numb2);
+            //Console.WriteLine(number_2 > number ? "The swapped value is indeed greater." : "The swapped value isnt greater...");
+            //Console.WriteLine(numb2);
+
+            Console.ReadKey();
+        }
+    }
+}
