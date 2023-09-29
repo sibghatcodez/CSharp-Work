53c76cb881e6541b99acf934e881e8bc8d5e57bf
sibghatdev81 <118879569+sibghatcodez@users.noreply.github.com>
2023-09-29 21:58:00 +0500

    Create Day_11.cs

---
+++ Day_11.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Day_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Day 11th
+           /* Console.WriteLine(Math.sum(100,69)); // Output: 169
+            Console.WriteLine(Math.sub(100, 69)); // Output: 31
+            Console.WriteLine(Math.mul(10, 10)); // Output: 100
+            Console.WriteLine(Math.div(10, 2)); // Output: 5
+            int[] array = new int[] { 10, 20, 30 };
+            Console.WriteLine(Math.avg(array)); // Output: 20 i think
+            Console.WriteLine(Math.sqrt(2)); // Output: 4 */
+
+            Console.ReadKey();
+        }
+    }
+    /* static class Math
+    {
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int sqrt(int a)
+        {
+            return a * a;
+        }
+        public static int avg(int[] ex)
+        {
+            int sum = 0;
+            foreach(var item in ex) { sum += item; }
+            return sum / ex.Length;
+        }
+        public static int mul(int a, int b)
+        {
+            return a * b;
+        }
+        public static int div(int a, int b)
+        {
+            return a / b;
+        }
+    } */
+}
